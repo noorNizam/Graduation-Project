bb854b140751519531c003c2fdf83b4462d27a04
NoorNizam <srsn3561@gmail.com>
2026-07-30 22:42:26 +0300

    fix(reverb): fix config

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     Illuminate\Broadcasting\BroadcastServiceProvider::class,
+    Laravel\Reverb\ApplicationManagerServiceProvider::class,
+    Laravel\Reverb\ReverbServiceProvider::class,
 ];
